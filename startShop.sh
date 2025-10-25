cefb5928757aadf4eaf0609e75b92fc43498fb5d
Piotr Kierznowski <s197652@student.pg.edu.pl>
2025-10-25 17:19:43 +0200

    stop tracking database files

--- startShop.sh
+++ startShop.sh
@@ -3,7 +3,7 @@
 echo "Starting prestashop"
 
 # start docker images
-docker-compose up
+docker-compose up -d
 
 # load sql dump
-docker-compose exec -T mysql mysql-u root -p"admin" prestashop < ./prestashop/dbdata/sqlDump.sql
+docker-compose exec -T mysql mysql -u root -p"admin" prestashop < ./prestashop/dbdata/sqlDump.sql
