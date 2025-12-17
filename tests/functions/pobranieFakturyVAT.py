8ecee7209a87c521d5bd31cd5bcd137956861730
Piotr Kierznowski <s197652@student.pg.edu.pl>
2025-12-17 16:59:17 +0100

    faktura

--- tests/functions/pobranieFakturyVAT.py
+++ tests/functions/pobranieFakturyVAT.py
@@ -2,29 +2,45 @@ from common import *
 
 
 def pobierz_fakture(driver):
+    wait = WebDriverWait(driver, 10)
     driver.get("https://localhost:8443/historia-zamowien")
 
-    mail = driver.find_element(By.CSS_SELECTOR, "input#field-email")
+    mail = wait.until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "input#field-email"))
+    )
     mail.clear()
     mail.send_keys("pub@prestashop.com")
 
-    password = driver.find_element(By.CSS_SELECTOR, "input#field-password")
+    password = wait.until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "input#field-password"))
+    )
     password.clear()
     password.send_keys("123456789")
 
-    driver.find_element(By.CSS_SELECTOR, "button#submit-login").click()
-    sleep(1)
+    submit_btn = wait.until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "button#submit-login"))
+    )
+    submit_btn.click()
 
-    table = driver.find_element(By.CSS_SELECTOR, "tbody")
-    rows = table.find_elements(By.CSS_SELECTOR, "tr")
+    try:
+        table_body = wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "tbody"))
+        )
+    except:
+        print("Brak tabeli zamówień lub timeout")
+        return
+
+    rows = table_body.find_elements(By.CSS_SELECTOR, "tr")
 
     for row in rows:
         cols = row.find_elements(By.CSS_SELECTOR, "td")
 
-        try:
-            cols[4].find_element(By.CSS_SELECTOR, "a").click()
-            print("pobrano fakture")
-        except:
-            print("brak faktury")
+        if len(cols) > 4:
+            try:
+                invoice_link = cols[4].find_element(By.TAG_NAME, "a")
 
-    sleep(1)
+                driver.execute_script("arguments[0].click();", invoice_link)
+                print("pobrano fakture")
+
+            except Exception:
+                print("brak faktury")
--- tests/main.py
+++ tests/main.py
@@ -19,21 +19,6 @@ driver = webdriver.Chrome(service=service, options=chrome_options)
 # dodanie_produktow_przez_wyszukiwarke(driver, phrase)
 # usun_produkty(driver)
 # wykoanie_zamowienia(driver)
-status_zamowienia(driver)
-#
-# while True:
-#     akcja = input("Wybierz akcje:")
-#
-#     if akcja == "1":
-#         testowanie_rejestracji(driver)
-#     elif akcja == "2":
-#         phrase = input("co wyszukac:")
-#         dodanie_produktow_przez_wyszukiwarke(driver, phrase)
-#     elif akcja == "3":
-#         usun_produkty(driver)
-#     elif akcja == "4":
-#         wykoanie_zamowienia(driver)
-#     elif akcja == "5":
-#         status_zamowienia(driver)
-#     elif akcja == "6":
-#         pobierz_fakture(driver)
+# status_zamowienia(driver)
+pobierz_fakture(driver)
+
