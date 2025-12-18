5934a3f0ddb9e2db6c6f4621d79d405b78f033a9
Piotr Kierznowski <s197652@student.pg.edu.pl>
2025-12-18 15:04:07 +0100

    skrypt do testowania

--- tests/main.py
+++ tests/main.py
@@ -10,16 +10,35 @@ from functions.sprawdzenieStatusuZamowienia import status_zamowienia
 from functions.usuniecieProduktow import usun_produkty
 from functions.wykonanieZamowienia import wykoanie_zamowienia, dodaj_do_koszyka_produkty
 
-service = Service()
-driver = webdriver.Chrome(service=service, options=chrome_options)
+
+def uruchom_test(funkcja_testowa, *args):
+    service = Service()
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    funkcja_testowa(driver, *args)
+    driver.quit()
 
 
-# testowanie_rejestracji(driver)
-# phrase = input("co wyszukac:")
-# phrase = "nab"
-# dodanie_produktow_przez_wyszukiwarke(driver, phrase)
-# usun_produkty(driver)
-# wykoanie_zamowienia(driver)
-# status_zamowienia(driver)
-# pobierz_fakture(driver)
-dodaj_do_koszyka_z_kategori(driver)
+# --- WYKONANIE TESTÓW ---
+
+# 1. Rejestracja
+uruchom_test(testowanie_rejestracji)
+
+# 2. Dodawanie przez wyszukiwarkę
+phrase = "nab"
+uruchom_test(dodanie_produktow_przez_wyszukiwarke, phrase)
+
+# 3. Usuwanie produktów
+uruchom_test(usun_produkty)
+
+# 4. Wykonanie zamówienia
+uruchom_test(wykoanie_zamowienia)
+
+# 5. Status zamówienia
+uruchom_test(status_zamowienia)
+
+# 6. Pobranie faktury
+uruchom_test(pobierz_fakture)
+
+# 7. Dodanie z kategorii
+uruchom_test(dodaj_do_koszyka_z_kategori)
+
