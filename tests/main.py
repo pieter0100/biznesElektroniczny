0986436c1b70446b0fbfc9f9485155dbe5e7a50f
Piotr Kierznowski <s197652@student.pg.edu.pl>
2026-01-08 11:40:40 +0100

    poprawa testu dodawania 10 produtkow do koszyka

--- tests/main.py
+++ tests/main.py
@@ -15,30 +15,32 @@ def uruchom_test(funkcja_testowa, *args):
     service = Service()
     driver = webdriver.Chrome(service=service, options=chrome_options)
     funkcja_testowa(driver, *args)
+    input()
     driver.quit()
 
 
 # --- WYKONANIE TESTÓW ---
 
 # 1. Rejestracja
-uruchom_test(testowanie_rejestracji)
+# uruchom_test(testowanie_rejestracji)
+#
+# # 2. Dodawanie przez wyszukiwarkę
+# phrase = "nab"
+# uruchom_test(dodanie_produktow_przez_wyszukiwarke, phrase)
+#
+# # 3. Usuwanie produktów
+# uruchom_test(usun_produkty)
+#
+# # 4. Wykonanie zamówienia
+# uruchom_test(wykoanie_zamowienia)
+#
+# # 5. Status zamówienia
+# uruchom_test(status_zamowienia)
+#
+# # 6. Pobranie faktury
+# uruchom_test(pobierz_fakture)
+#
+# # 7. Dodanie z kategorii
+# uruchom_test(dodaj_do_koszyka_z_kategori)
 
-# 2. Dodawanie przez wyszukiwarkę
-phrase = "nab"
-uruchom_test(dodanie_produktow_przez_wyszukiwarke, phrase)
-
-# 3. Usuwanie produktów
-uruchom_test(usun_produkty)
-
-# 4. Wykonanie zamówienia
-uruchom_test(wykoanie_zamowienia)
-
-# 5. Status zamówienia
-uruchom_test(status_zamowienia)
-
-# 6. Pobranie faktury
-uruchom_test(pobierz_fakture)
-
-# 7. Dodanie z kategorii
 uruchom_test(dodaj_do_koszyka_z_kategori)
-
